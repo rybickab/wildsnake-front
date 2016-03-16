cb6e30a6b8bcb45cef1ecfe1eb9881c7837c8215
Jaroslaw Dlugosz <jaroslaw.dlugosz@gmail.com>
2016-03-16 13:21:45 +0100

    poc_version

---
+++ src/main/java/tech/allegro/config/BatchConfig.java
@@ -0,0 +1,92 @@
+package tech.allegro.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import tech.allegro.domain.Product;
+import tech.allegro.io.twitter.config.TwitterAccessProperties;
+import tech.allegro.io.twitter.reader.TwitterStreamItemReader;
+import tech.allegro.mapper.ProductItemProcessor;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public DataSource dataSource;
+
+    @Autowired
+    private TwitterAccessProperties accessProperties;
+
+    // tag::readerwriterprocessor[]
+    @Bean
+    public TwitterStreamItemReader<String> reader() {
+        return new TwitterStreamItemReader(accessProperties);
+    }
+
+    @Bean
+    public ProductItemProcessor processor() {
+        return new ProductItemProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Product> writer() {
+        JdbcBatchItemWriter<Product> writer = new JdbcBatchItemWriter<Product>();
+        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Product>());
+        writer.setSql("INSERT INTO product (name, description, image_url, price) VALUES (:name, :description, :image_url, :price)");
+        writer.setDataSource(dataSource);
+        return writer;
+    }
+    // end::readerwriterprocessor[]
+
+    // tag::listener[]
+
+    @Bean
+    public JobExecutionListener listener() {
+        return new JobProductImportExecutionListener(new JdbcTemplate(dataSource));
+    }
+
+    // end::listener[]
+
+    // tag::jobstep[]
+    @Bean
+    public Job importProductsJob() {
+        return jobBuilderFactory.get("importProductsJob")
+                .incrementer(new RunIdIncrementer())
+                .listener(listener())
+                .flow(consumeTweeterStream())
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step consumeTweeterStream() {
+        return stepBuilderFactory.get("consumeTweeterStream")
+                .<String, Product>chunk(10)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+    // end::jobstep[]
+}
