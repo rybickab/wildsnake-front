0aec981881a193c1f50f0e4558e37a9fb1be30ba
Jaroslaw Dlugosz <jaroslaw.dlugosz@gmail.com>
2016-03-16 01:05:57 +0100

    remove directories

--- build/resources/main/schema-all.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE product IF EXISTS;
-
-CREATE TABLE product  (
-    id BIGINT IDENTITY PRIMARY KEY,
-    name VARCHAR(20),
-    description VARCHAR(10000),
-    image_url VARCHAR (250),
-    price DECIMAL
-);
