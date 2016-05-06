dd878c4a2e149337aaa8f81ede966fab661ad411
Michał Jezierski <ambinanitelo@gmail.com>
2016-05-06 10:08:56 +0200

    production api

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,5 +1,5 @@
 function wildsnakeCtrl($http) {
-  $http.get('http://localhost:8080/api/v1/products').then(response => this.products = response.data);
+  $http.get('https://wildsnake-jjdlugosz.herokuapp.com/api/v1/products').then(response => this.products = response.data);
 }
 
 function wildsnakeConfig ($interpolateProvider) {
