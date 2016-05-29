3fabb2284e73216948cc7f1bc322720bed207eda
rybickab <12benia@gmail.com>
2016-05-29 22:07:34 +0200

    remove duplicates

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8085;
 
 
 app.use(express.static('static'));
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -8,5 +8,25 @@ function wildsnakeConfig ($interpolateProvider) {
 
 angular.module('wildsnake', [])
   .config(wildsnakeConfig)
-  .controller('WildsnakeCtrl', wildsnakeCtrl);
+  .controller('WildsnakeCtrl', wildsnakeCtrl)
+  .filter('unique', function() {
+    return function(collection, keyname1, keyname2, keyname3, keyname4) {
+      var output = [],
+          keys = [];
+
+      angular.forEach(collection, function(item) {
+        var key1 = item[keyname1];
+        var key2 = item[keyname2];
+        var key3 = item[keyname3];
+        var key4 = item[keyname4];
+        var key = key1 + key2 + key3 + key4;
+        if(keys.indexOf(key) === -1) {
+          keys.push(key);
+          output.push(item);
+        }
+      });
+
+      return output;
+    };
+  });
 
